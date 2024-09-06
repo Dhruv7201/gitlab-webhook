3afda8571f09d4cf56dc4154914ac60419ee9ea3
Dhruv7201 <dhruv.modi2345@gmail.com>
2024-09-06 17:57:03 +0530

    changed notification logic

--- frontend/src/Notification.tsx
+++ frontend/src/Notification.tsx
@@ -6,29 +6,25 @@ type NotificationProps = {
 };
 
 const Notification = ({ message, type }: NotificationProps) => {
-  // if notification exists, dont create new one
-  if (
-    document.getElementsByClassName(
-      "Toastify__toast Toastify__toast-theme--light Toastify__toast--error"
-    ).length > 0
-  ) {
-    return;
-  }
-
   switch (type) {
     case "info":
+      toast.dismiss();
       toast.info(message, { autoClose: 3000, pauseOnHover: false });
       break;
     case "success":
+      toast.dismiss();
       toast.success(message, { autoClose: 3000, pauseOnHover: false });
       break;
     case "error":
+      toast.dismiss();
       toast.error(message, { autoClose: 3000, pauseOnHover: false });
       break;
     default:
+      toast.dismiss();
       toast.info(message, { autoClose: 3000, pauseOnHover: false });
       break;
   }
 };
 
 export default Notification;
+
