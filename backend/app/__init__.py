efe34b47b43f7316c8b94062c5597867d3479e4f
Dhruv7201 <dhruv.modi2345@gmail.com>
2024-11-04 18:42:30 +0530

    added ip block middleware

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,8 +4,6 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import JSONResponse
 
 
-
-
 app = FastAPI()
 
 app.add_middleware(
@@ -18,7 +16,7 @@ app.add_middleware(
 # Define the IPs you want to block
 BLOCKED_IPS = {"192.168.0.121", "192.168.0.202"}
 
-class BlockIPMiddleware(BaseHTTPMiddleware):
+class BlockIPMiddleware(BaseHTTPMiddleware): # this will be used to block the IP addresses it will give the 403 forbidden error
     async def dispatch(self, request: Request, call_next):
         client_ip = request.client.host
         if client_ip in BLOCKED_IPS:
@@ -34,10 +32,14 @@ class BlockIPMiddleware(BaseHTTPMiddleware):
         return response
 
 # Add the middleware to the FastAPI app
-app.add_middleware(BlockIPMiddleware)
+app.add_middleware(BlockIPMiddleware) # adding the middleware to the app for blocking the IP addresses
 
 
-
+''' 
+this will be used to register the routers to the app
+importing here to avoid circular imports
+'''
 from app.routes import register_routers
 
 register_routers(app)
+
